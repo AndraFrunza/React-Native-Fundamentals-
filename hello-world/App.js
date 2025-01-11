4aa4c503c218cfe3d5443563032c6c361c521ece
Andra Frunza <andraaafrunza@gmail.com>
2025-01-11 23:32:52 +0200

    added View, Image and ScrollView tags

--- hello-world/App.js
+++ hello-world/App.js
@@ -1,13 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Image } from "react-native";
+
+//const image = {uri: 'https://example.com/cool-image.jpg'};
+const image = require("./react-native.jpg"); //faster than uri attribute if we have a lower internet connection
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello World</Text>
+      {/* <Text>Hello World</Text>
       <Text>My name is Andra</Text>
       <Text>This is my first React Native App</Text>
-      <StatusBar style="auto" />
+
+      <Text style={{ fontSize: 16 }}>The 
+        <Text style={{ fontWeight: 'bold' }}>quick brown fox</Text> 
+        jumps over the lazy dog
+      </Text>
+
+      <Image source={image} style={{ width: 100, height: 100 }}/>
+
+      <StatusBar style="auto" /> */}
+
+      <Text style={{ fontSize: 24, textAlign: "center" }}>Scroll me!</Text>
+      <View style={{ height: 400, backgroundColor: "#e5e5e5" }}>
+        <ScrollView
+          style={{ width: 300, height: 300, backgroundColor: "red" }}
+        />
+        <ScrollView
+          style={{ width: 300, height: 300, backgroundColor: "green" }}
+        />
+        <ScrollView
+          style={{ width: 300, height: 300, backgroundColor: "blue" }}
+        />
+      </View>
     </View>
   );
 }
@@ -15,8 +39,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'red',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "red",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
