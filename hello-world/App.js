66ce6650bd8478cb8b7885bcc888f5fb97e32ac3
Andra Frunza <andraaafrunza@gmail.com>
2025-02-03 23:01:17 +0200

    Press a button

--- hello-world/App.js
+++ hello-world/App.js
@@ -1,46 +1,26 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, Image } from "react-native";
+import React, { useState } from "react";
+import { Button, Text, View } from "react-native";
 
-//const image = {uri: 'https://example.com/cool-image.jpg'};
-const image = require("./react-native.jpg"); //faster than uri attribute if we have a lower internet connection
+const App = () => {
+  const [pressedCount, setPressedCount] = useState(0);
 
-export default function App() {
   return (
-    <View style={styles.container}>
-      {/* <Text>Hello World</Text>
-      <Text>My name is Andra</Text>
-      <Text>This is my first React Native App</Text>
-
-      <Text style={{ fontSize: 16 }}>The 
-        <Text style={{ fontWeight: 'bold' }}>quick brown fox</Text> 
-        jumps over the lazy dog
+    <View
+      style={{ flex: 1, justifyContent: "center", backgroundColor: "white" }}
+    >
+      <Text style={{ margin: 16 }}>
+        {pressedCount > 0
+          ? `The button was pressed ${pressedCount} times!`
+          : "The button isn't pressed yet"}
       </Text>
-
-      <Image source={image} style={{ width: 100, height: 100 }}/>
-
-      <StatusBar style="auto" /> */}
-
-      <Text style={{ fontSize: 24, textAlign: "center" }}>Scroll me!</Text>
-      <View style={{ height: 400, backgroundColor: "#e5e5e5" }}>
-        <ScrollView
-          style={{ width: 300, height: 300, backgroundColor: "red" }}
-        />
-        <ScrollView
-          style={{ width: 300, height: 300, backgroundColor: "green" }}
-        />
-        <ScrollView
-          style={{ width: 300, height: 300, backgroundColor: "blue" }}
-        />
-      </View>
+      <Button
+        title="Press me"
+        onPress={() => setPressedCount(pressedCount + 1)}
+        disabled={pressedCount >= 3}
+      />
+      <Button title="Reset count" onPress={() => setPressedCount(0)} />
     </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "red",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+export default App;
